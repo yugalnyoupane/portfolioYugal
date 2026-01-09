f7e6c81b70aa6662452fb713e1237e1641470694
Yugal Nyoupane <147706135+yugalnyoupane@users.noreply.github.com>
2026-01-09 22:43:44 +0545

    Change base path in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolioYugal/', // your GitHub repo name
+  base: '/', // your GitHub repo name
   plugins: [react()],
 })
